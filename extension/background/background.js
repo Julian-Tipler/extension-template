f105616704b0bf563844e638c66ec6fefbe9eb4a
Julian <tipler.julian@gmail.com>
2025-08-10 20:40:18 -0500

    basic draggable component works

--- extension/background/background.js
+++ extension/background/background.js
@@ -1,1 +1,10 @@
-console.log("background 🥷")
+console.log("background 🥷");
+
+// Initialize with default settings if not already set
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.get(["corner"], function(result) {
+    if (!result.corner) {
+      chrome.storage.local.set({ corner: "top-right" });
+    }
+  });
+});
